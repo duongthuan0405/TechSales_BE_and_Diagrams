d7f207286da3f44f05ffcd500c1c1c8fda0ce5cb
duongthuan0405 <duongthuan0405@gmail.com>
2026-05-13 23:03:34 +0700

    FIX: Lock when login fail too much

--- TechSalesManagement/Program.cs
+++ TechSalesManagement/Program.cs
@@ -18,7 +18,11 @@ public class Program
         // Add services to the container.
         builder.Services.AddAuthenticationConfiguration(builder.Configuration);
         builder.Services.AddAuthorization();
-        builder.Services.AddControllers();
+        builder.Services.AddControllers()
+            .AddJsonOptions(options =>
+            {
+                options.JsonSerializerOptions.Converters.Add(new System.Text.Json.Serialization.JsonStringEnumConverter());
+            });
 
         // Configure Swagger via Extension Method
         builder.Services.AddSwaggerConfiguration();
