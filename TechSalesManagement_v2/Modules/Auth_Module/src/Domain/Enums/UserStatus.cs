7cfe1df9b412cee8227026e742e8f2511b55026e
duongthuan0405 <duongthuan0405@gmail.com>
2026-06-29 21:01:13 +0700

    ADD: Domain/Entity User

--- TechSalesManagement_v2/Modules/Auth_Module/src/Domain/Enums/UserStatus.cs
+++ TechSalesManagement_v2/Modules/Auth_Module/src/Domain/Enums/UserStatus.cs
@@ -1,14 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
 namespace Auth_Module.src.Domain.Enums
 {
     public enum UserStatus
     {
-        PENDING = 0,
-        ACTIVE = 1,
-        BLOCKED = 2
+        PENDING,
+        ACTIVE,
+        BLOCKED,
+        DELETED
     }
 }
